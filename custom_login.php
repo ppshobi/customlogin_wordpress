a73ba28fdfdf3cb3d36df1fa2e3687203a71029f
Shobi P P <ppshobi@gmail.com>
2016-05-03 17:14:28 +0530

    added plugin meta information

--- custom_login.php
+++ custom_login.php
@@ -1,4 +1,11 @@
 <?php
-
-
+/**
+ * Plugin Name: Wordpress Custom Logins
+ * Plugin URI: https://github.com/ppshobi/customlogin_wordpress
+ * Description: A wordpress plugin allows users to customize the login process and front end.
+ * Version: 0.0.1
+ * Author: Shobi P P
+ * Author URI: https://github.com/ppshobi
+ * License: GPL2
+ */
 ?>
